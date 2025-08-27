24c63f6e39cbd5a948da7dd16682d065f8fbbce4
Ibne-Shahid <anasbinshahid2004@gmail.com>
2025-08-27 16:56:14 +0600

    Heart Icon function added

---
+++ js-files/script.js
@@ -0,0 +1,9 @@
+// Heart Icon Numbers
+
+function heartIcon (){
+
+    const heartCount = parseInt(document.getElementById("heart-count").innerText)
+    
+    document.getElementById("heart-count").innerText = heartCount + 1
+
+}
